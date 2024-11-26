27107ed507479d1a75c255e4253f9cc23c55601b
adityapandey9882 <ap9113783@gmail.com>
2024-11-26 22:21:47 +0530

    backend-code

--- index.js
+++ index.js
@@ -9,10 +9,17 @@ import connectDB from "./config/connectDB.js"
 import userRouter from './route/user.route.js'
 
 const app = express()
+// app.use(cors({
+//     credentials: true,
+//     origin : process.env.FRONTEND_URL
+// }))
 app.use(cors({
-    credentials: true,
-    origin : process.env.FRONTEND_URL
+    origin : "https:blinkit-ecommerc-application.netlify.app",
+    method : ['GET', 'POST', 'PUT', 'DELETE'],
+    credentials : true
 }))
+
+
 app.use(express.json())
 app.use(cookieParser())
 app.use(morgan())
